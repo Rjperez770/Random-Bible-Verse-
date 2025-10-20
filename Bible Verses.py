5d727393b15f3f850590a7fa187414a7addac274
Bobby Perez <rjperez770@gmail.com>
2025-10-20 14:04:08 -0500

    Added first bible verse that relates to the feeling of being anxious

---
+++ Bible Verses.py
@@ -0,0 +1,13 @@
+import random
+from textwrap import fill
+
+#The bible verses that are grouped by feelings
+verse_blank = {
+  "anxious": [
+    {
+      "reference": "Philippians 4:6-7",
+      "text": "Do not be anxious about anything, but in every situation, by prayer and petition, with thanksgiving, present your requests to God. And the peace of God, which transcends all understanding, will guard your hearts and your minds in Christ Jesus.",
+      "description": "When worry runs high, prayer and thanksgiving invite God's peace into your heart"
+    },
+  ]
+}
