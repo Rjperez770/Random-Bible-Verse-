a20076d29e55c7b36c34004ffb68cc79935e1c9f
Bobby Perez <rjperez770@gmail.com>
2025-10-21 16:09:22 -0500

    added all different feeling that user can choose from

--- Bible Verses.py
+++ Bible Verses.py
@@ -9,5 +9,26 @@ verse_blank = {
       "text": "Do not be anxious about anything, but in every situation, by prayer and petition, with thanksgiving, present your requests to God. And the peace of God, which transcends all understanding, will guard your hearts and your minds in Christ Jesus.",
       "description": "When worry runs high, prayer and thanksgiving invite God's peace into your heart"
     },
+  ],
+  "lonely": [
+
+  ],
+  "grateful":[
+
+  ],
+  "broken":[
+
+  ],
+  "tempted":[
+
+  ],
+  "hopeless":[
+
+  ],
+  "joyful":[
+
+  ],
+  "fearful":[
+    
   ]
 }
